3780c6684e60837502425a209e193c9eeaf6ca72
Jagat05 <joshijagaths@gmail.com>
2026-01-17 08:41:22 +0545

    fix(next.config): update image config for Vercel deploy

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,7 +2,20 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['fakestoreapi.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fakestoreapi.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
 };
 
