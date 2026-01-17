aa6a1a8592182d8e5eaf499c224d02539da18376
Jagat05 <joshijagaths@gmail.com>
2026-01-17 14:01:32 +0545

    Fix(Admin):Admin Layout Fixed

--- client/app/admin/layout.tsx
+++ client/app/admin/layout.tsx
@@ -3,11 +3,9 @@ import AdminSidebar from "./components/adminsidebar";
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-      <div className="flex">
-        <AdminSidebar />
-        {children}
-      </div>
+    <div className="flex min-h-screen">
+      <AdminSidebar />
+      <main className="flex-1 overflow-y-auto">{children}</main>
     </div>
   );
 };
--- client/app/admin/page.tsx
+++ client/app/admin/page.tsx
@@ -1,7 +1,17 @@
+"use client";
 import React from "react";
 
 const AdminDashboard = () => {
-  return <div>AdminDashboard</div>;
+  return (
+    <div className="flex-1 min-h-screen bg-gray-100 p-8">
+      <div className="bg-indigo-600 p-8 rounded-3xl text-white">
+        <h2 className="text-2xl font-bold mb-2">Welcome to Admin Dashboard</h2>
+        <p className="text-indigo-100">
+          Manage products, orders, users, and settings from here.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default AdminDashboard;
